219c5225f3ec5d7dcc750e73fa8883cdd9f8699e
sertion1126 <20447647@qq.com>
2018-05-16 10:58:45 +0800

    课程模块借口更新5.16

--- volador/src/main/java/org/ranji/lemon/volador/service/course/impl/ClassifyServiceImpl.java
+++ volador/src/main/java/org/ranji/lemon/volador/service/course/impl/ClassifyServiceImpl.java
@@ -6,6 +6,9 @@ import java.util.Map;
 import org.ranji.lemon.core.pagination.PagerModel;
 import org.ranji.lemon.core.service.impl.GenericServiceImpl;
 import org.ranji.lemon.volador.model.course.Classify;
+import org.ranji.lemon.volador.model.course.Course;
+import org.ranji.lemon.volador.persist.course.prototype.IClassifyDao;
+import org.ranji.lemon.volador.persist.course.prototype.ICourseDao;
 import org.ranji.lemon.volador.service.course.prototype.IClassifyService;
 import org.ranji.lemon.volador.service.course.prototype.ICourseService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +16,26 @@ import org.springframework.stereotype.Service;
 
 @Service("VoladorClassifyServiceImpl")
 public class ClassifyServiceImpl extends GenericServiceImpl<Classify, Integer> implements IClassifyService {
+	
     @Autowired
     private IClassifyService  classifyService;
+
+	@Override
+	public void saveClassifyAndCourseRelation(int course_id, int classify_id) {
+		// TODO Auto-generated method stub
+		 ((IClassifyDao) dao).saveClassifyAndCourseRelation(course_id, classify_id);
+	}
+
+	@Override
+	public void deleteClassifyAndCourseRelation(int course_id, int classify_id) {
+		// TODO Auto-generated method stub
+		((IClassifyDao) dao).deleteClassifyAndCourseRelation(course_id, classify_id);
+	}
+
+	@Override
+	public List<Course> findCourseByClassify(int classify_id) {
+		// TODO Auto-generated method stub
+		return ((IClassifyDao) dao).findCoursebyClassify(classify_id);
+	}
     
 }
--- volador/src/main/java/org/ranji/lemon/volador/service/course/prototype/IClassifyService.java
+++ volador/src/main/java/org/ranji/lemon/volador/service/course/prototype/IClassifyService.java
@@ -1,8 +1,33 @@
 package org.ranji.lemon.volador.service.course.prototype;
 
+import java.util.List;
+
 import org.ranji.lemon.core.persist.prototype.IGenericDao;
+import org.ranji.lemon.volador.model.course.Chapter;
 import org.ranji.lemon.volador.model.course.Classify;
+import org.ranji.lemon.volador.model.course.Course;
 
 public interface IClassifyService extends IGenericDao<Classify, Integer> {
+	
+    /**
+     * 存储课程分类与课程关系
+     * @param course_id
+     * @param classify_id
+     */
+    public void saveClassifyAndCourseRelation(int course_id,int classify_id);
+
+    /**
+     * 删除课程分类与课程的对应关系
+     * @param course_id
+     * @param classify_id
+     */
+    public void deleteClassifyAndCourseRelation(int course_id,int classify_id);
+    
+    /**
+     * 根据课程分类ID查找Course
+     * @param classify_id 课程分类ID
+     * @return 课程列表
+     */
+    public List<Course> findCourseByClassify(int classify_id);
 
 }
