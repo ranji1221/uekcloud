e10a95365a87967b51579f7c7d517246b5561f78
pfdpugongying <514819817@qq.com>
2018-05-22 18:46:23 +0800

    公告服务接口

---
+++ volador/src/main/java/org/ranji/lemon/volador/service/global/prototype/IAnnouncementService.java
@@ -0,0 +1,8 @@
+package org.ranji.lemon.volador.service.global.prototype;
+
+import org.ranji.lemon.core.service.prototype.IGenericService;
+import org.ranji.lemon.volador.model.global.Announcement;
+
+public interface IAnnouncementService extends IGenericService<Announcement, Integer>{
+
+}
