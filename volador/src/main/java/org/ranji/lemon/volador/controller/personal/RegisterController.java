f8192a5d589fb8c1b54407143ae1ed3ddf8691c2
gongfanfan <1056684096@qq.com>
2018-05-22 18:21:53 +0800

    注册功能添加异常信息处理显示 注册界面验证码实现

--- volador/src/main/java/org/ranji/lemon/volador/controller/personal/RegisterController.java
+++ volador/src/main/java/org/ranji/lemon/volador/controller/personal/RegisterController.java
@@ -1,22 +1,25 @@
 package org.ranji.lemon.volador.controller.personal;
 
-
+import java.io.PrintWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.ranji.lemon.volador.model.personal.Per;
 import org.ranji.lemon.volador.model.personal.UserInfo;
 import org.ranji.lemon.volador.service.personal.prototype.IPerService;
 import org.ranji.lemon.volador.service.personal.prototype.IUserInfoService;
+import org.ranji.lemon.volador.util.SmsBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class RegisterController {
@@ -46,44 +49,54 @@ public class RegisterController {
 		
 		ModelAndView mv = new ModelAndView();
 		
-		//输入参数有效性检查
-		Boolean isLegal = CheckValid(username, password1, code);
-		if(!isLegal || !password1.equals(password2)){
-			mv.addObject("message", "Parameter illega");
-			mv.setViewName("redirect:/register");
-			return mv;
-		}
-		//查看用户是否已经注册
-		Per user = personalService.findByUserName(username);
-		if(null != user){
-			mv.addObject("message", "用户已经注册");	
-			mv.setViewName("redirect:/register");
-		}
-		else{
-			//保存用户
-			user = new Per();
-			user.setUsername(username);
-			user.setPassword(password1);
-			try{
-				personalService.save(user);
-				
-				//保存用户对应的用户信息，默认为空
-				UserInfo userInfo = new UserInfo();
-				
-				//设置默认头像
-				userInfo.setHead_image("photos\\wqf_user.png");
-				userInfoService.save(userInfo);
-				
-				//保存用户关系表
-				personalService.saveUserAndUserInfoRelation(user.getId(), userInfo.getId());
-				
-				mv.setViewName("redirect:/login");
-			} catch (Exception e){
-				mv.addObject("message", "register errors");
-				mv.setViewName("redirect:/register");
+		try{
+			//输入参数有效性检查
+			Boolean isLegal = CheckValid(username, password1);
+			if(!isLegal || !password1.equals(password2)){
+				mv.addObject("message", "用户名或密码错误！");
+
+			}else if(!checkCode(request.getSession().getAttribute("code").toString(), code)){
+				//验证短信验证码
+				mv.addObject("message", "验证码错误!");
+
 			}
+			else{
+				//查看用户是否已经注册
+				Per user = personalService.findByUserName(username);
+				if(null != user){
+					mv.addObject("message", "用户已经注册");	
+
+				}else{				
+					//保存用户
+					user = new Per();
+					user.setUsername(username);
+					user.setPassword(password1);
+					
+	                personalService.save(user);
+					
+					//保存用户对应的用户信息，默认为空
+					UserInfo userInfo = new UserInfo();
+					
+					//设置默认头像
+					userInfo.setHead_image("photos\\wqf_user.png");
+					userInfoService.save(userInfo);
+					
+					//保存用户关系表
+					personalService.saveUserAndUserInfoRelation(user.getId(), userInfo.getId());
+					mv.setViewName("redirect:/login");	
+					//用户注册成功，返回登录就界面
+					return mv;					
+				}
+			}
+			
+		} catch (RuntimeException e){
+			mv.addObject("message", "短信验证失败!");
+			e.printStackTrace();
+		}catch (Exception e){
+			mv.addObject("message", "注册异常：注册用户失败!");
+			e.printStackTrace();
 		}
-				
+		mv.setViewName("/backend/cp_register");
 		return mv;
 	}
 
@@ -94,7 +107,7 @@ public class RegisterController {
 	 * @param code      验证码
 	 * @return 参数合法，返回true，否则返回false
 	 */
-	public Boolean CheckValid(String username, String password, String code){
+	public Boolean CheckValid(String username, String password){
 		
 		//验证用户名是否合法
 		String regEx = "^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\\d{8}$";
@@ -116,4 +129,91 @@ public class RegisterController {
 		
 		return true;
 	}
+	
+	
+	@RequestMapping(value = "/register_sms", method = RequestMethod.POST)
+	public void sms(HttpServletRequest request, HttpServletResponse response) throws Exception {      
+        
+        String result = "0";      
+        /** 手机号码 */      
+        Object jbPhone=request.getParameter("jbPhone");     
+        System.out.println("jbphone------------------------"+jbPhone);  
+        /** 短信验证码 */      
+        Object code = request.getParameter("code");  
+        System.out.println("code---------------------------"+code);  
+        /** 短信验证码存入session(session的默认失效时间30分钟) */      
+        request.getSession().setAttribute("code", code.toString());      
+     
+        /** 单个手机号发送短信的方法的参数准备 */      
+        // 手机号码      
+        String mobilephone = jbPhone.toString();      
+        // 短信内容+随机生成的6位短信验证码      
+        String content = "【飞鱼平台】注册验证码为:" + code.toString();//自定义发送的内容  </span>  
+            
+            /** 单个手机号发送短信 */      
+           if (!sendMessage(content, mobilephone)) {      
+                result = "0";// 失败      
+            } else {      
+                result = "1";// 成功      
+                }      
+        
+        PrintWriter out = response.getWriter();      
+        out.write(result.toString());     
+    }
+	
+	public static boolean sendMessage( String content, String mobileNumber) {      
+        SmsBase smsBase = new SmsBase();  
+       // 单个手机号码发送      
+       try {      
+           String  retObj = smsBase.SendSms(mobileNumber, content);  
+           System.out.println("in sendMessage -------------------------");  
+           //System.out.println(retObj);    
+           if (retObj == "未发送，编码异常") {       
+               return false;      
+           } else {      
+               return true;      
+           }     
+       } catch (Exception ex) {      
+           ex.printStackTrace();       
+       }      
+       return true;      
+   }  
+	/**
+	 * 验证短信验证码是否正确 
+	 * @param smsCheckCode   手动输入的手机短信验证码
+	 * @param code           session中存放的手机短信验证码
+	 * @return               验证码正确返回True
+	 */
+	public Boolean checkCode(String smsCheckCode, String code){
+		Boolean isValidCode = false;
+		try{
+			if(!smsCheckCode.isEmpty() && smsCheckCode.equals(code.toString())){
+				isValidCode = true;
+			}
+		}catch (Exception e) {
+			throw new RuntimeException("短信验证失败", e);
+		}
+		return isValidCode;
+	}
+	/*
+	// 验证短信验证码是否正确     
+	@RequestMapping(value = "/register_checkCode", method = RequestMethod.POST)
+    public void checkCode(HttpServletRequest request, HttpServletResponse response) throws Exception{      
+        String result = "0";      
+        // 获取手动输入的手机短信验证码     
+        String SmsCheckCode = (String)(request.getParameter("SmsCheckCode"));       
+        // 获取session中存放的手机短信验证码      
+        Object code =request.getSession().getAttribute("code");      
+        try {      
+            if(SmsCheckCode != code.toString() && !SmsCheckCode.equals(code.toString())){      
+                result = "0";      
+            }else{      
+                result = "1";      
+            }      
+        } catch (Exception e) {      
+            throw new RuntimeException("短信验证失败", e);      
+        }       
+        PrintWriter out = response.getWriter();      
+        out.write(result.toString());      
+    }*/
 }
---
+++ volador/src/main/java/org/ranji/lemon/volador/util/SmsBase.java
@@ -0,0 +1,302 @@
+package org.ranji.lemon.volador.util;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder; 
+
+/**
+ * 手机验证码工具类
+ * @author 范小亚
+ *
+ */
+public class SmsBase
+{
+	private String x_id="XXXXXXX";
+	private String x_pwd="XXXXXXX";
+	public String SendSms(String mobile,String content) throws UnsupportedEncodingException{  
+	    Integer x_ac=10;//发送信息  
+	    HttpURLConnection httpconn = null;  
+	    String result="Error";  
+	    StringBuilder sb = new StringBuilder();  
+	    sb.append("http://service.winic.org:8009/sys_port/gateway/index.asp?");  
+	      
+	    //以下是参数  
+	    sb.append("id=").append(URLEncoder.encode(x_id, "gb2312"));  
+	    sb.append("&pwd=").append(x_pwd);  
+	    sb.append("&to=").append(mobile);  
+	    sb.append("&content=").append(URLEncoder.encode(content, "gb2312"));   
+	    sb.append("&time=").append("1");  
+	    try {  
+	        URL url = new URL(sb.toString());  
+	        httpconn = (HttpURLConnection) url.openConnection();  
+	        BufferedReader rd = new BufferedReader(new InputStreamReader(httpconn.getInputStream()));  
+	        result = rd.readLine();  
+	        rd.close();  
+	        } catch (MalformedURLException e) {  
+	            // TODO Auto-generated catch block  
+	            e.printStackTrace();  
+	        } catch (IOException e) {  
+	            // TODO Auto-generated catch block  
+	            e.printStackTrace();  
+	        } finally{  
+	            if(httpconn!=null){  
+	                httpconn.disconnect();  
+	                httpconn=null;  
+	            }  
+	      
+	        }  
+	        return result;  
+	    }  
+	/*
+	private static final Logger Log = LoggerFactory.getLogger(MobileServerUtils.class);
+	private String serviceURL = "";
+	private String sn = "";         //序列号
+	private String password = "";
+	private String pwd = "";
+	
+	
+	
+	 * 构造函数
+	 
+	private MobileServerUtils(String serviceURL, String sn, String password) throws UnsupportedEncodingException {
+		this.serviceURL = serviceURL;
+		this.sn = sn;
+		this.password = password;
+		//密码为md(sn+password)
+		this.pwd = this.getMD5(sn + password);
+	}
+
+
+	
+	 * 方法名称：getMD5
+	 * 功       能：字符串MD5加密
+	 * 参       数：带转换的字符串
+	 * 返  回  值：加密之后的字符串
+	 
+	private String getMD5(String sourceStr) throws UnsupportedEncodingException{
+		String resultStr = "";
+		try{
+			byte[] temp = sourceStr.getBytes();
+			MessageDigest md5 = MessageDigest.getInstance("MD5");
+			md5.update(temp);
+			
+			byte[] b = md5.digest();
+			for(int i = 0; i < b.length; i++){
+				char[] digit = {'0', '1', '2', '3', '4', '5', '6', '7', '8',  
+                        '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+				char[] ob = new char[2];
+				ob[0] = digit[(b[i] >>> 4 & 0X0F)];
+				ob[1] = digit[b[i] & 0X0F];
+				resultStr += new String(ob);
+			}
+			return resultStr;
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}		
+	}
+	
+	
+	 
+     * 方法名称：mdgetSninfo  
+     * 功    能：获取信息 
+     * 参    数：sn,pwd(软件序列号，加密密码md5(sn+password)) 
+     *  
+     
+	private String mdgetSninfo() {  
+        String result = "";  
+        String soapAction = "http://entinfo.cn/mdgetSninfo";  
+        String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";  
+        xml += "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">";  
+        xml += "<soap:Body>";  
+        xml += "<mdgetSninfo xmlns=\"http://entinfo.cn/\">";  
+        xml += "<sn>" + sn + "</sn>";  
+        xml += "<pwd>" + pwd + "</pwd>";  
+        xml += "</mdgetSninfo>";  
+        xml += "</soap:Body>";  
+        xml += "</soap:Envelope>";  
+  
+        URL url;  
+        try {  
+            url = new URL(serviceURL);  
+  
+            URLConnection connection = url.openConnection();  
+            HttpURLConnection httpconn = (HttpURLConnection) connection;  
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();  
+            bout.write(xml.getBytes());  
+            byte[] b = bout.toByteArray();  
+            httpconn.setRequestProperty("Content-Length", String  
+                    .valueOf(b.length));  
+            httpconn.setRequestProperty("Content-Type",  
+                    "text/xml; charset=gb2312");  
+            httpconn.setRequestProperty("SOAPAction", soapAction);  
+            httpconn.setRequestMethod("POST");  
+            httpconn.setDoInput(true);  
+            httpconn.setDoOutput(true);  
+  
+            OutputStream out = httpconn.getOutputStream();  
+            out.write(b);  
+            out.close();  
+  
+            InputStreamReader isr = new InputStreamReader(httpconn  
+                    .getInputStream());  
+            BufferedReader in = new BufferedReader(isr);  
+            String inputLine;  
+            while (null != (inputLine = in.readLine())) {  
+                Pattern pattern = Pattern.compile("<mdgetSninfoResult>(.*)</mdgetSninfoResult>");  
+                Matcher matcher = pattern.matcher(inputLine);  
+                while (matcher.find()) {  
+                    result = matcher.group(1);  
+                }  
+            }  
+            return result;  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+            return "";  
+        }  
+    }
+	
+	 
+     * 方法名称：mdgxsend  
+     * 功    能：发送个性短信  
+     * 参    数：mobile,content,ext,stime,rrid,msgfmt(手机号，内容，扩展码，定时时间，唯一标识，内容编码) 
+     * 返 回 值：唯一标识，如果不填写rrid将返回系统生成的 
+       
+    private String mdgxsend(String mobile, String content, String ext, String stime,  
+            String rrid, String msgfmt) {  
+        String result = "";  
+        String soapAction = "http://entinfo.cn/mdgxsend";  
+        String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";  
+        xml += "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">";  
+        xml += "<soap:Body>";  
+        xml += "<mdgxsend xmlns=\"http://entinfo.cn/\">";  
+        xml += "<sn>" + sn + "</sn>";  
+        xml += "<pwd>" + pwd + "</pwd>";  
+        xml += "<mobile>" + mobile + "</mobile>";  
+        xml += "<content>" + content + "</content>";  
+        xml += "<ext>" + ext + "</ext>";  
+        xml += "<stime>" + stime + "</stime>";  
+        xml += "<rrid>" + rrid + "</rrid>";  
+        xml += "<msgfmt>" + msgfmt + "</msgfmt>";  
+        xml += "</mdgxsend>";  
+        xml += "</soap:Body>";  
+        xml += "</soap:Envelope>";  
+  
+        URL url;  
+        try {  
+            url = new URL(serviceURL);  
+  
+            URLConnection connection = url.openConnection();  
+            HttpURLConnection httpconn = (HttpURLConnection) connection;  
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();  
+            bout.write(xml.getBytes());  
+            byte[] b = bout.toByteArray();  
+            httpconn.setRequestProperty("Content-Length", String  
+                    .valueOf(b.length));  
+            httpconn.setRequestProperty("Content-Type",  
+                    "text/xml; charset=gb2312");  
+            httpconn.setRequestProperty("SOAPAction", soapAction);  
+            httpconn.setRequestMethod("POST");  
+            httpconn.setDoInput(true);  
+            httpconn.setDoOutput(true);  
+  
+            OutputStream out = httpconn.getOutputStream();  
+            out.write(b);  
+            out.close();  
+
+            InputStreamReader isr = new InputStreamReader(httpconn  
+                    .getInputStream());  
+            BufferedReader in = new BufferedReader(isr);  
+            String inputLine;  
+            while (null != (inputLine = in.readLine())) {  
+                Pattern pattern = Pattern.compile("<mdgxsendResult>(.*)</mdgxsendResult>");  
+                Matcher matcher = pattern.matcher(inputLine);  
+                while (matcher.find()) {  
+                    result = matcher.group(1);  
+                }  
+            }  
+            return result;  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+            return "";  
+        }  
+    }  
+      
+      
+    *//** 
+     * 方法名称：mdsmssend 
+     * 功    能：发送短信  
+     * @param mobile 手机号 
+     * @param content 内容 
+     * @param ext 扩展码 
+     * @param stime 定时时间 
+     * @param rrid 唯一标识 
+     * @param msgfmt 内容编码 
+     * @return 唯一标识，如果不填写rrid将返回系统生成的 
+     *//*  
+    private String mdsmssend(String mobile, String content, String ext, String stime,  
+            String rrid,String msgfmt) {  
+        String result = "";  
+        String soapAction = "http://entinfo.cn/mdsmssend";  
+        String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";  
+        xml += "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">";  
+        xml += "<soap:Body>";  
+        xml += "<mdsmssend  xmlns=\"http://entinfo.cn/\">";  
+        xml += "<sn>" + sn + "</sn>";  
+        xml += "<pwd>" + pwd + "</pwd>";  
+        xml += "<mobile>" + mobile + "</mobile>";  
+        xml += "<content>" + content + "</content>";  
+        xml += "<ext>" + ext + "</ext>";  
+        xml += "<stime>" + stime + "</stime>";  
+        xml += "<rrid>" + rrid + "</rrid>";  
+        xml += "<msgfmt>" + msgfmt + "</msgfmt>";  
+        xml += "</mdsmssend>";  
+        xml += "</soap:Body>";  
+        xml += "</soap:Envelope>";  
+  
+        URL url;  
+        try {  
+            url = new URL(serviceURL);  
+  
+            URLConnection connection = url.openConnection();  
+            HttpURLConnection httpconn = (HttpURLConnection) connection;  
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();  
+            //bout.write(xml.getBytes());  
+            bout.write(xml.getBytes("GBK"));  
+            byte[] b = bout.toByteArray();  
+            httpconn.setRequestProperty("Content-Length", String  
+                    .valueOf(b.length));  
+            httpconn.setRequestProperty("Content-Type",  
+                    "text/xml; charset=gb2312");  
+            httpconn.setRequestProperty("SOAPAction", soapAction);  
+            httpconn.setRequestMethod("POST");  
+            httpconn.setDoInput(true);  
+            httpconn.setDoOutput(true);  
+  
+            OutputStream out = httpconn.getOutputStream();  
+            out.write(b);  
+            out.close();  
+  
+            InputStreamReader isr = new InputStreamReader(httpconn  
+                    .getInputStream());  
+            BufferedReader in = new BufferedReader(isr);  
+            String inputLine;  
+            while (null != (inputLine = in.readLine())) {  
+                Pattern pattern = Pattern.compile("<mdsmssendResult>(.*)</mdsmssendResult>");  
+                Matcher matcher = pattern.matcher(inputLine);  
+                while (matcher.find()) {  
+                    result = matcher.group(1);  
+                }  
+            }  
+            return result;  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+            return "";  
+        }  
+    } */ 
+}
